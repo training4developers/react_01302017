6c5850949a3246a2196169c82907f32eb604de45
Eric W. Greene <eric@ericwgreene.com>
2017-01-30 15:11:23 -0500

    added code demos

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'bootstrap-loader';
-import '../css/styles.scss';
+const colors = ['green','yellow','black','red','white','blue'];
 
-class HelloWorld extends React.Component {
+class ColorList extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            newColor: ''
+        };
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({
+            [e.currentTarget.name]: e.currentTarget.value
+        });
+    }
 
     render() {
-        return <h1>Hello World!</h1>;
+
+        return <div>
+            <h1>Color List</h1>
+            <ul>
+                {this.props.myColors.map(myColor =>
+                    <li>{myColor}</li>
+                )}
+            </ul>
+            <form>
+                <div>
+                    <label htmlFor="new-color-input">New Color</label>
+                    <input type="text" id="new-color-input" name="newColor"
+                        value={this.state.newColor} onChange={this.onChange} />
+                </div>
+            </form>
+        </div>;
     }
 }
 
-ReactDOM.render( <HelloWorld /> , document.querySelector('main'));
+ReactDOM.render(<ColorList myColors={colors} />, document.querySelector('main'));
