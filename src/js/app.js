b50fd26068317efd4ee7402211eb7eef2f15e017
Eric W. Greene <eric@ericwgreene.com>
2017-01-31 10:57:11 -0500

    decomposed color tool

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,32 @@ import ReactDOM from 'react-dom';
 
 const colors = ['green','yellow','black','red','white','blue'];
 
-class ColorList extends React.Component {
+class ColorHeader extends React.Component {
+    render() {
+        return <h1>Color Tool</h1>;
+    }
+}
+
+class ItemList extends React.Component {
+
+    static propTypes = {
+        items: React.PropTypes.array
+    };
+
+    render() {
+        return <ul>
+            {this.props.items.map(item =>
+                <li>{item}</li>
+            )}
+        </ul>;
+    }
+}
+
+class ColorForm extends React.Component {
+
+    static propTypes = {
+        addColor: React.PropTypes.func.isRequired
+    }
 
     constructor(props) {
         super(props);
@@ -11,34 +36,62 @@ class ColorList extends React.Component {
         this.state = {
             newColor: ''
         };
-
-        //this.onChange = this.onChange.bind(this);
     }
 
     onChange = (e) => {
         this.setState({
             [e.currentTarget.name]: e.currentTarget.value
         });
+    };
+
+    addNewColor = () => {
+        this.props.addColor(this.state.newColor);
+        this.setState({
+            newColor: ''
+        });
+    };
+
+    render() {
+        return <form>
+            <div>
+                <label htmlFor="new-color-input">New Color</label>
+                <input type="text" id="new-color-input" name="newColor"
+                    value={this.state.newColor} onChange={this.onChange} />
+            </div>
+            <button type="button" onClick={this.addNewColor}>Add Color</button>
+        </form>;
+    }    
+
+}
+
+class ColorTool extends React.Component {
+
+    static propTypes = {
+        myColors: React.PropTypes.array
+    };    
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            colors: this.props.myColors.concat()
+        };
+    }
+
+    addColor = (newColor) => {
+        this.setState({
+            colors: this.state.colors.concat(newColor)
+        });
     }
 
     render() {
 
         return <div>
-            <h1>Color List</h1>
-            <ul>
-                {this.props.myColors.map(myColor =>
-                    <li>{myColor}</li>
-                )}
-            </ul>
-            <form>
-                <div>
-                    <label htmlFor="new-color-input">New Color</label>
-                    <input type="text" id="new-color-input" name="newColor"
-                        value={this.state.newColor} onChange={() => this.onChange()} />
-                </div>
-            </form>
+            <ColorHeader />
+            <ItemList items={this.state.colors} />
+            <ColorForm addColor={this.addColor} />
         </div>;
     }
 }
 
-ReactDOM.render(<ColorList myColors={colors} />, document.querySelector('main'));
+ReactDOM.render(<ColorTool myColors={colors} />, document.querySelector('main'));
